9698e8e1c78b118e2eefe5051722ce4d2343a697
Rodrigo Schuck <sschuckk@gmail.com>
2023-11-09 18:49:35 -0500

    Add test case 'list_tasks' and use UUID lib to create random values for payload

--- py_test_api/tests/test_api_integration.py
+++ py_test_api/tests/test_api_integration.py
@@ -1,4 +1,5 @@
 import requests
+import uuid
 
 ENDPOINT = 'https://todo.pixegami.io'
 
@@ -11,21 +12,26 @@ def test_get_api():
 
 
 def test_create_task():
-    response = create_task(new_task_payload())
+    # Create a task
+    payload = new_task_payload()
+    response = create_task(payload)
     assert response.status_code == 200
     data = response.json()
     task_id = data['task']['task_id']
+
+    # Get and validate the values
     response = get_task(task_id)
     assert response.status_code == 200
     data = response.json()
     assert data['task_id'] == task_id
-    assert data['user_id'] == new_task_payload()["user_id"]
-    assert data['content'] == new_task_payload()["content"]
+    assert data['user_id'] == payload["user_id"]
+    assert data['content'] == payload["content"]
 
 
 def test_update_task():
+    payload = new_task_payload()
     # Create a task
-    response = create_task(new_task_payload())
+    response = create_task(payload)
     assert response.status_code == 200
     data = response.json()
     task_id = data['task']['task_id']
@@ -33,7 +39,7 @@ def test_update_task():
     # Update the task
     updated_task_payload = {
         "task_id": task_id,
-        "user_id": new_task_payload()["user_id"],
+        "user_id": payload["user_id"],
         "content": "Task content updated!",
         "is_done": True,
     }
@@ -44,15 +50,34 @@ def test_update_task():
     response = get_task(task_id)
     assert response.status_code == 200
     data = response.json()
+    assert data['task_id'] == updated_task_payload['task_id']
+    assert data['user_id'] == updated_task_payload['user_id']
+    assert data['content'] == updated_task_payload['content']
+    assert data['is_done'] == updated_task_payload['is_done']
     print(data)
 
+
+def test_list_tasks():
+    # Create N tasks under same user_id
+    n = 10
+    payload = new_task_payload()
+    for _ in range(n):
+        response = create_task(payload)
+        assert response.status_code == 200
+
+    # List tasks, and check items
+    response = list_tasks(payload['user_id'])
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data['tasks']) == n
+    print(data)
 
 def get_task(task_id):
     return requests.get(ENDPOINT + f'/get-task/{task_id}')
 
 
-def list_task(user_id):
-    return requests.get(ENDPOINT + f'/get-task/{user_id}')
+def list_tasks(user_id):
+    return requests.get(ENDPOINT + f'/list-tasks/{user_id}')
 
 
 def create_task(payload):
@@ -64,8 +89,10 @@ def update_task(payload):
 
 
 def new_task_payload():
+    user_id = f"test_user_{uuid.uuid4().hex}"  # Return a string hex representation a a user_id
+    content = f"test_content_{uuid.uuid4().hex}"
     return {
-        "content": "Default task content",
-        "user_id": "user_001",
-        "is_done": False,
+        'content': content,
+        'user_id': user_id,
+        'is_done': False
     }
