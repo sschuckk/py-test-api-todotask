4cd1ccbd5816bceb1fb69cf5a782f6014bafed76
Rodrigo Schuck <sschuckk@gmail.com>
2023-11-10 12:08:19 -0500

    Improve variables readability Remove debug prints

--- py_test_api/tests/test_api_integration.py
+++ py_test_api/tests/test_api_integration.py
@@ -11,27 +11,25 @@ def test_get_api():
 def test_create_task():
     # Create a task
     payload = new_task_payload()
-    response = create_task(payload)
-    assert response.status_code == 200
-    data = response.json()
-    task_id = data['task']['task_id']
+    create_task_response = create_task(payload)
+    assert create_task_response.status_code == 200
+    task_id = create_task_response.json()['task']['task_id']
 
     # Get and validate the values
-    response = get_task(task_id)
-    assert response.status_code == 200
-    data = response.json()
+    get_task_response = get_task(task_id)
+    assert get_task_response.status_code == 200
+    data = get_task_response.json()
     assert data['task_id'] == task_id
     assert data['user_id'] == payload["user_id"]
     assert data['content'] == payload["content"]
 
 
 def test_update_task():
-    payload = new_task_payload()
     # Create a task
-    response = create_task(payload)
-    assert response.status_code == 200
-    data = response.json()
-    task_id = data['task']['task_id']
+    payload = new_task_payload()
+    create_task_response = create_task(payload)
+    assert create_task_response.status_code == 200
+    task_id = create_task_response.json()['task']['task_id']
 
     # Update the task
     updated_task_payload = {
@@ -40,18 +38,17 @@ def test_update_task():
         "content": "Task content updated!",
         "is_done": True,
     }
-    response = update_task(updated_task_payload)
-    assert response.status_code == 200
+    update_task_response = update_task(updated_task_payload)
+    assert update_task_response.status_code == 200
 
     # Get and validate the changes
-    response = get_task(task_id)
-    assert response.status_code == 200
-    data = response.json()
+    get_task_response = get_task(task_id)
+    assert get_task_response.status_code == 200
+    data = get_task_response.json()
     assert data['task_id'] == updated_task_payload['task_id']
     assert data['user_id'] == updated_task_payload['user_id']
     assert data['content'] == updated_task_payload['content']
     assert data['is_done'] == updated_task_payload['is_done']
-    print(data)
 
 
 def test_list_tasks():
@@ -59,28 +56,27 @@ def test_list_tasks():
     n = 10
     payload = new_task_payload()
     for _ in range(n):
-        response = create_task(payload)
-        assert response.status_code == 200
+        create_task_response = create_task(payload)
+        assert create_task_response.status_code == 200
 
     # List tasks, and check items
-    response = list_tasks(payload['user_id'])
-    assert response.status_code == 200
-    data = response.json()
+    list_tasks_response = list_tasks(payload['user_id'])
+    assert list_tasks_response.status_code == 200
+    data = list_tasks_response.json()
     assert len(data['tasks']) == n
-    print(data)
 
 
 def test_delete_task():
-    payload = new_task_payload()
     # Create task
-    response = create_task(payload)
-    assert response.status_code == 200
-    task_id = response.json()['task']['task_id']
+    payload = new_task_payload()
+    create_task_response = create_task(payload)
+    assert create_task_response.status_code == 200
+    task_id = create_task_response.json()['task']['task_id']
 
     # Delete task
-    response = delete_task(task_id)
-    assert response.status_code == 200
+    delete_task_response = delete_task(task_id)
+    assert delete_task_response.status_code == 200
 
     # Get and verify task
-    response = get_task(task_id)
-    assert response.status_code == 404
+    get_task_response = get_task(task_id)
+    assert get_task_response.status_code == 404
