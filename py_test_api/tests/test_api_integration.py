49c701c55336fe5e08f8a7f9c822fa2342e3febb
Rodrigo Schuck <sschuckk@gmail.com>
2023-11-11 09:13:58 -0500

    Add docstring with test case description

--- py_test_api/tests/test_api_integration.py
+++ py_test_api/tests/test_api_integration.py
@@ -5,14 +5,17 @@ logger = logging.getLogger(__name__)
 
 
 def test_create_task():
-
-    # Create a task
+    """
+    This test case checks whether the create_task function successfully creates a task by sending a new task payload.
+    It then verifies that the created task has the expected values by retrieving and validating the task details.
+    """
+    # Create a new task
     payload = new_task_payload()
     create_task_response = create_task(payload)
     assert create_task_response.status_code == 200
     task_id = create_task_response.json()['task']['task_id']
 
-    # Get and validate the values
+    # Get and verify the task details
     get_task_response = get_task(task_id)
     assert get_task_response.status_code == 200
     data = get_task_response.json()
@@ -22,7 +25,11 @@ def test_create_task():
 
 
 def test_update_task():
-    # Create a task
+    """
+    This test case checks whether the update_task function successfully updates the details of an existing task.
+    It involves creating a task, updating its content and status, and then verifying the changes.
+    """
+    # Create a new task
     payload = new_task_payload()
     create_task_response = create_task(payload)
     assert create_task_response.status_code == 200
@@ -38,7 +45,7 @@ def test_update_task():
     update_task_response = update_task(updated_task_payload)
     assert update_task_response.status_code == 200
 
-    # Get and validate the changes
+    # Get and verify the changes
     get_task_response = get_task(task_id)
     assert get_task_response.status_code == 200
     data = get_task_response.json()
@@ -49,14 +56,19 @@ def test_update_task():
 
 
 def test_list_tasks():
-    # Create N tasks under same user_id
+    """
+    This test case checks whether the list_tasks function successfully retrieves a list of tasks for a given user_id.
+    It involves creating a specified number of tasks under the same user_id and then verifying that the listed tasks
+    match the expected count.
+    """
+    # Create N new tasks under same user_id. For this endpoint the limit is 10
     n = 10
     payload = new_task_payload()
     for _ in range(n):
         create_task_response = create_task(payload)
         assert create_task_response.status_code == 200
 
-    # List tasks, and check items
+    # List tasks, and check the number of tasks
     list_tasks_response = list_tasks(payload['user_id'])
     assert list_tasks_response.status_code == 200
     data = list_tasks_response.json()
@@ -64,16 +76,20 @@ def test_list_tasks():
 
 
 def test_delete_task():
-    # Create task
+    """
+    This test case verifies whether the delete_task function successfully removes a task by creating a task,
+    deleting it, and then attempting to retrieve it to ensure it no longer exists.
+    """
+    # Create a new task
     payload = new_task_payload()
     create_task_response = create_task(payload)
     assert create_task_response.status_code == 200
     task_id = create_task_response.json()['task']['task_id']
 
-    # Delete task
+    # Delete the created task
     delete_task_response = delete_task(task_id)
     assert delete_task_response.status_code == 200
 
-    # Get and verify task
+    # Get and verify if the task exist
     get_task_response = get_task(task_id)
     assert get_task_response.status_code == 404
